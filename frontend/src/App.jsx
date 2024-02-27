04886b0c1381102cee0bb73100327fc170924076
Sam <burtman.samuel26@gmail.com>
2024-02-27 14:44:16 +0200

    update src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import About from './pages/about/About'
+import Alcohol from './pages/alcohol/Alcohol'
+import Contact from './pages/contact/Contact'
+import Gallery from './pages/gallery/Gallery'
+import Home from './pages/home/Home'
+import NotFound from './pages/notFound/NotFound'
+import Plans from './pages/plans/Plans'
+
 const App = () => {
-	return <div>Portary Website</div>
+	return (
+		<div>
+			<Home />
+			<About />
+			<Contact />
+			<Gallery />
+			<Gallery />
+			<Plans />
+			<Alcohol />
+			<NotFound />
+		</div>
+	)
 }
 
 export default App
