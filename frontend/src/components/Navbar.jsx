c7dcd2a039727f1744850ccd43aa3f52d685ad5d
Sam <burtman.samuel26@gmail.com>
2024-03-01 17:03:48 +0200

    update frontend/src/components/

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import './navbar.css'
+
+const Navbar = () => {
+	return (
+		<nav>
+			<div className='.container nav__container'>
+				logo nav links nav buttons
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar
