81093853a0479d5fa749e58f8d982813a5f851fa
Sam <burtman.samuel26@gmail.com>
2024-03-03 22:19:04 +0200

    update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/about/About'
 import Alcohol from './pages/alcohol/Alcohol'
 import Contact from './pages/contact/Contact'
@@ -5,10 +6,12 @@ import Gallery from './pages/gallery/Gallery'
 import Home from './pages/home/Home'
 import NotFound from './pages/notFound/NotFound'
 import Plans from './pages/plans/Plans'
+import Navbar from './components/Navbar'
 
 const App = () => {
 	return (
-		<div>
+		<BrowserRouter>
+		  <Navbar/>
 			<Home />
 			<About />
 			<Contact />
@@ -17,7 +20,7 @@ const App = () => {
 			<Plans />
 			<Alcohol />
 			<NotFound />
-		</div>
+		</BrowserRouter>
 	)
 }
 
