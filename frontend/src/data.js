060b71ba31636283531dc51261f97ac49f04685a
Sam <burtman.samuel26@gmail.com>
2024-03-03 22:18:45 +0200

    add data.js

---
+++ frontend/src/data.js
@@ -0,0 +1,26 @@
+export const links = [
+	{
+		name: 'Home',
+		path: '/',
+	},
+	{
+		name: 'About',
+		path: '/about',
+	},
+	{
+		name: 'Gallery',
+		path: '/gallery',
+	},
+	{
+		name: 'Plans',
+		path: '/plans',
+	},
+	{
+		name: 'Alcohol',
+		path: '/alcohol',
+	},
+	{
+		name: 'Contact',
+		path: '/contact',
+	},
+]
