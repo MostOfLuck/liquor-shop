bfdc53878f6487cc67725b3cd0c4e11bcf24b3df
Sam <burtman.samuel26@gmail.com>
2024-03-03 22:19:17 +0200

    update frontend/src/components/Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,28 @@
+import { Link, NavLink } from 'react-router-dom'
 import './navbar.css'
+import Logo from '../images/logo.png'
+import { links } from '../data.js'
+import { AiOutlineBars } from "react-icons/ai";
 
 const Navbar = () => {
 	return (
 		<nav>
 			<div className='.container nav__container'>
-				logo nav links nav buttons
+				<Link to={'/'} className='logo'>
+					<img src={Logo} alt='Nav logo' />
+				</Link>
+				<ul className='nav__links'>
+					{links.map(({ name, path }, index) => {
+						return (
+							<li>
+								<NavLink to={path}>{name}</NavLink>
+							</li>
+						)
+					})}
+				</ul>
+				<button className="nav__toggle-btn">
+				<AiOutlineBars />
+				</button>
 			</div>
 		</nav>
 	)
