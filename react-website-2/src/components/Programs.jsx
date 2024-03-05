5c6ce3e4be2dc696fc2947e5862e06546621ee35
Sam <burtman.samuel26@gmail.com>
2024-03-05 21:54:54 +0200

    update react-website-2/src/components/Programs.jsx

--- react-website-2/src/components/Programs.jsx
+++ react-website-2/src/components/Programs.jsx
@@ -1,24 +1,25 @@
 import {FaCrown} from 'react-icons/fa'
 import SectionHead from './SectionHead'
+import Image from '../images/wine&flowers.png'
 import {programs} from '../data'
 import Card from '../UI/Card'
 import {Link} from 'react-router-dom'
 import {AiFillCaretRight} from 'react-icons/ai'
 
 
+
 const Programs = () => {
   return (
     <section className="programs">
         <div className="container programs__container">
-            <SectionHead icon={<FaCrown/>} title="Programs"/>
+            <SectionHead icon={<FaCrown/>} title="Catalog"/>
         <div className="programs__wrapper">
             {
-                programs.map(({id, icon, title, info, path}) => {
+                programs.map(({id, title, path}) => {
                     return (
                         <Card className="programs__program" key={id}>
-                            <span>{icon}</span>
                             <h4>{title}</h4>
-                            <small>{info}</small>
+                            <img src={Image} alt="product__image" />
                             <Link to={path} className="btn sm">Learn More <AiFillCaretRight/></Link>
                         </Card>
                     )
