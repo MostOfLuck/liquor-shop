660e2bdca34dbcea1685473f8d1b8f874e3a82bd
Sam <burtman.samuel26@gmail.com>
2024-03-07 20:31:11 +0200

    update src/App.jsx

--- react-website-2/src/App.jsx
+++ react-website-2/src/App.jsx
@@ -5,10 +5,10 @@ import About from './pages/about/About'
 import Contact from './pages/contact/Contact'
 import Gallery from './pages/gallery/Gallery'
 import Plans from './pages/plans/Plans'
-import Trainers from './pages/trainers/Trainers'
 import NotFound from './pages/notFound/NotFound'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Catalogs from './pages/trainers/Trainers'
 
 
 const App = () => {
@@ -17,11 +17,11 @@ const App = () => {
       <Navbar/>
       <Routes>
         <Route index element={<Home/>}/>
-        <Route path='about' element={<About/>}/>
         <Route path='contact' element={<Contact/>}/>
+        <Route path='about' element={<About/>}/>
         <Route path='gallery' element={<Gallery/>}/>
         <Route path='plans' element={<Plans/>}/>
-        <Route path='trainers' element={<Trainers/>}/>
+        <Route path='catalog' element={<Catalogs/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
       <Footer/>
