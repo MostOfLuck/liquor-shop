0b84d67dfa31a9f8d7f52dfb2ee5d170ee19b733
Sam <burtman.samuel26@gmail.com>
2024-03-08 13:49:24 +0200

    update src/data.js

--- react-website-2/src/data.js
+++ react-website-2/src/data.js
@@ -261,84 +261,57 @@ export const plans = [
 	},
 ]
 
-const Product1 = require('./productImages2/774A7548.jpg')
-const Trainer2 = require('./images/trainer2.jpg')
-const Trainer3 = require('./images/trainer3.jpg')
-const Trainer4 = require('./images/trainer4.jpg')
-const Trainer5 = require('./images/trainer5.jpg')
-const Trainer6 = require('./images/trainer6.jpg')
 
-export const trainers = [
-	{
-		id: 1,
-		image: Product1,
-		name: 'Vine',
-		job: 'Italic Vine',
-		socials: [
-			'https://instagram.com/',
-			'https://twitter.com/',
-			'https://facebook.com/',
-			'https://linkedin.com/',
-		],
-	},
-	{
-		id: 2,
-		image: Trainer2,
-		name: 'Daniel vinyo',
-		job: 'Speed Trainer',
-		socials: [
-			'https://instagram.com/',
-			'https://twitter.com/',
-			'https://facebook.com/',
-			'https://linkedin.com/',
-		],
-	},
-	{
-		id: 3,
-		image: Trainer3,
-		name: 'Edem Quist',
-		job: 'Flexibility Trainer',
-		socials: [
-			'https://instagram.com/',
-			'https://twitter.com/',
-			'https://facebook.com/',
-			'https://linkedin.com/',
-		],
-	},
-	{
-		id: 4,
-		image: Trainer4,
-		name: 'Shatta Wale',
-		job: 'Body Composition Trainer',
-		socials: [
-			'https://instagram.com/',
-			'https://twitter.com/',
-			'https://facebook.com/',
-			'https://linkedin.com/',
-		],
-	},
-	{
-		id: 5,
-		image: Trainer5,
-		name: 'Diana Ayi',
-		job: 'Circuit Trainer',
-		socials: [
-			'https://instagram.com/',
-			'https://twitter.com/',
-			'https://facebook.com/',
-			'https://linkedin.com/',
-		],
-	},
-	{
-		id: 6,
-		image: Trainer6,
-		name: 'Wayne Carter',
-		job: 'Physical Intelligence Trainer',
-		socials: [
-			'https://instagram.com/',
-			'https://twitter.com/',
-			'https://facebook.com/',
-			'https://linkedin.com/',
-		],
-	},
-]
+let products = {
+    data: [
+      {
+        productName: "Regular White T-Shirt",
+        category: "Topwear",
+        price: "30",
+        image: "white-tshirt.jpg",
+      },
+      {
+        productName: "Beige Short Skirt",
+        category: "Bottomwear",
+        price: "49",
+        image: "short-skirt.jpg",
+      },
+      {
+        productName: "Sporty SmartWatch",
+        category: "Watch",
+        price: "99",
+        image: "sporty-smartwatch.jpg",
+      },
+      {
+        productName: "Basic Knitted Top",
+        category: "Topwear",
+        price: "29",
+        image: "knitted-top.jpg",
+      },
+      {
+        productName: "Black Leather Jacket",
+        category: "Jacket",
+        price: "129",
+        image: "black-leather-jacket.jpg",
+      },
+      {
+        productName: "Stylish Pink Trousers",
+        category: "Bottomwear",
+        price: "89",
+        image: "pink-trousers.jpg",
+      },
+      {
+        productName: "Brown Men's Jacket",
+        category: "Jacket",
+        price: "189",
+        image: "brown-jacket.jpg",
+      },
+      {
+        productName: "Comfy Gray Pants",
+        category: "Bottomwear",
+        price: "49",
+        image: "comfy-gray-pants.jpg",
+      },
+    ],
+  };
+  
