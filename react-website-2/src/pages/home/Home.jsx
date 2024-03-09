13b4bebb3f129113cfa6cc6a73dd5ee0d48b92e2
Sam <burtman.samuel26@gmail.com>
2024-03-09 20:09:51 +0200

    update src/pages/home/Home.jsx

--- react-website-2/src/pages/home/Home.jsx
+++ react-website-2/src/pages/home/Home.jsx
@@ -1,21 +1,16 @@
-import FAQs from '../../components/FAQs'
-import Footer from '../../components/Footer'
 import MainHeader from '../../components/MainHeader'
 import Programs from '../../components/Programs'
 import Testimonials from '../../components/Testimonials'
-import Values from '../../components/Values'
-
-
 
 import './home.css'
 const Home = () => {
-  return (
-    <>
-    <MainHeader/>
-    <Programs/>
-    <Testimonials/>
-    </>
-  )
+	return (
+		<>
+			<MainHeader />
+			<Programs />
+			<Testimonials />
+		</>
+	)
 }
 
 export default Home
