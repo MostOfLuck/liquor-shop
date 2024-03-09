079bd8cdd2169ad38b026fd20f8672708524ad11
Sam <burtman.samuel26@gmail.com>
2024-03-09 20:08:11 +0200

    update src/components/Footer.jsx

--- react-website-2/src/components/Footer.jsx
+++ react-website-2/src/components/Footer.jsx
@@ -27,25 +27,18 @@ const Footer = () => {
                 </div>
             </article>
             <article>
-                <h4>Permalinks</h4>
-                <Link to="/about">About</Link>
-                <Link to="/plans">Plans</Link>
-                <Link to="/trainers">Trainers</Link>
-                <Link to="/gallery">Gallery</Link>
+                <h4 className='permalinks'> Permalinks</h4>
+                <Link to="/catalog">Catalog</Link>
                 <Link to="/contact">Contact</Link>
             </article>
             <article>
-                <h4>Insights</h4>
+                <h4 className='Insights'>Insights</h4>
                 <Link to="/s">Blog</Link>
-                <Link to="/s">Case Studies</Link>
-                <Link to="/s">Events</Link>
                 <Link to="/s">Communities</Link>
-                <Link to="/s">FAQs</Link>
             </article>
             <article>
-                <h4>Get In Touch</h4>
+                <h4 className='get-in-touch'>Get In Touch</h4>
                 <Link to="/contact">Contact Us</Link>
-                <Link to="/s">Support</Link>
             </article>
         </div>
         <div className="footer__copyright">
