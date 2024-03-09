50bac59361b133926fae1f8911746c62c4477259
Sam <burtman.samuel26@gmail.com>
2024-03-09 14:32:28 +0200

    update react-website-2/src/pages/trainers/Trainers.jsx

--- react-website-2/src/pages/trainers/Trainers.jsx
+++ react-website-2/src/pages/trainers/Trainers.jsx
@@ -1,22 +1,63 @@
-import React from 'react';
-import Header from '../../components/Header';
-import HeaderImage from '../../productImages/WhatsApp Image 2024-03-03 at 16.04.01.jpeg';
-import '../../data'; // Я не уверен, зачем импортирован этот файл. Если он не нужен, можно удалить эту строку.
-import './trainers.css';
+import React, { useState, useEffect } from 'react'
+import Header from '../../components/Header'
+import '../../data'
+import HeaderImage from '../../productImages/WhatsApp Image 2024-03-03 at 16.04.01.jpeg'
+import './trainers.css'
 
 const Catalogs = () => {
-  return (
-    <>
-      <Header title="Our Catalog" image={HeaderImage}>
-        Facilis, iusto numquam unde laboriosam expedita qui exercitationem? Dicta vero accusantium est aut molestiae fugit doloremque suscipit quod.
-      </Header>
-      <section className="catalog">
-        <div className="container catalog__container">
-          {/* Ваш дальнейший JSX-код */}
-        </div>
-      </section>
-    </>
-  );
-};
+	const products = [
+		{ id: 1, name: 'Vine', price: 99.99 },
+		{ id: 2, name: 'Vodka', price: 49.99 },
+		{ id: 3, name: 'Tequila', price: 49.99 },
+		{ id: 4, name: 'Martini', price: 49.99 },
+		{ id: 5, name: 'Спортивные шорты', price: 49.99 },
+		{ id: 6, name: 'Спортивные шорты', price: 49.99 },
+		{ id: 7, name: 'Спортивные шорты', price: 49.99 },
+		{ id: 8, name: 'Спортивные шорты', price: 49.99 },
+		// Добавьте другие товары сюда
+	]
 
-export default Catalogs;
+	const [searchTerm, setSearchTerm] = useState("");
+	const [searchResults, setSearchResults] = useState(products);
+
+	const handleChange = event => {
+		setSearchTerm(event.target.value);
+	};
+
+	useEffect(() => {
+		const results = products.filter(product =>
+			product.name.toLowerCase().includes(searchTerm.toLowerCase())
+		);
+		setSearchResults(results);
+	}, [searchTerm]);
+
+	return (
+		<>
+			<Header title='Our Catalog' image={HeaderImage}>
+				Facilis, iusto numquam unde laboriosam expedita qui exercitationem?
+				Dicta vero accusantium est aut molestiae fugit doloremque suscipit quod.
+			</Header>
+			<section className='catalog'>
+				<div className='container catalog__container'>
+					<input
+          className='placeholder'
+						type="text"
+						placeholder="Search for your product"
+						value={searchTerm}
+						onChange={handleChange}
+					/>
+					<div className='product-grid'>
+						{searchResults.map(product => (
+							<div key={product.id} className='product-card'>
+								<h3>{product.name}</h3>
+								<p>Price: ${product.price}</p>
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
+		</>
+	)
+}
+
+export default Catalogs
