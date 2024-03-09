a25a53cbff132c8883e618f7c8b734c85e6d485a
Sam <burtman.samuel26@gmail.com>
2024-03-09 20:07:46 +0200

    src/App.jsx update

--- react-website-2/src/App.jsx
+++ react-website-2/src/App.jsx
@@ -3,8 +3,6 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import Contact from './pages/contact/Contact'
-import Gallery from './pages/gallery/Gallery'
-import Plans from './pages/plans/Plans'
 import NotFound from './pages/notFound/NotFound'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
@@ -19,8 +17,6 @@ const App = () => {
         <Route index element={<Home/>}/>
         <Route path='contact' element={<Contact/>}/>
         <Route path='about' element={<About/>}/>
-        <Route path='gallery' element={<Gallery/>}/>
-        <Route path='plans' element={<Plans/>}/>
         <Route path='catalog' element={<Catalogs/>}/>
         <Route path='*' element={<NotFound/>}/>
       </Routes>
