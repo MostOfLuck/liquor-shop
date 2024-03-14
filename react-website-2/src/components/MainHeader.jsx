557517d5895e5d90f0badeadfd24b11ecb8b7fdc
Sam <burtman.samuel26@gmail.com>
2024-03-14 21:13:00 +0200

    update src/components/MainHeader.jsx

--- react-website-2/src/components/MainHeader.jsx
+++ react-website-2/src/components/MainHeader.jsx
@@ -1,18 +1,20 @@
 import { Link } from 'react-router-dom'
 import Image from '../images/wine1.png'
+import { useTranslation } from 'react-i18next';
 
 
 const MainHeader = () => {
+  const { t } = useTranslation();
   return (
     <header className="main__header">
       <div className="container main__header-container">
         <div className="main__header-left">
           <h4>#instadrink</h4>
-          <h1>Discover the Finest Alcoholic Beverages at R.I.L.L Collection</h1>
+          <h1>{t('Discover the Finest Alcoholic Beverages at R.I.L.L Collection')}</h1>
           <p>
-          Best Alcohol from all over the world
+          {t('Best Alcohol from all over the world')}
           </p>
-          <Link to="/Catalog" className='btn lg'>View Pruduct</Link>
+          <Link to="/Catalog" className='btn lg'>{t('Open Catalog')}</Link>
         </div>
         <div className="main__header-right">
           <div className="main__header-circle"></div>
