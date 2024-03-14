ac0126645945095d24214f7d68a288fd19b6f00b
Sam <burtman.samuel26@gmail.com>
2024-03-14 21:13:48 +0200

    update src/index.js

--- react-website-2/src/index.js
+++ react-website-2/src/index.js
@@ -1,6 +1,42 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      English: {
+        translations: { /* ваш перевод на английский язык */ }
+      },
+      Русский: {
+        translations: {
+					"Learn More": "Узнать больше",
+					"Discover the Finest Alcoholic Beverages at R.I.L.L Collection": "Откройте для себя лучшие алкогольные напитки в R.I.L.L Collection",
+					"Best Alcohol from all over the world": "Лучший алкоголь со всего мира",
+					"Open Catalog": "Открыть Каталог",
+					"Catalog": "Каталог",
+				}
+      },
+      עברית: {
+        translations: { /* ваш перевод на иврит */ }
+      }
+    },
+    lng: "English",
+    fallbackLng: "English",
+    ns: ["translations"],
+    defaultNS: "translations",
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+      formatSeparator: ","
+    },
+    react: {
+      wait: true
+    }
+  });
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
