a5320b3b708271daac1c9e564aca742f8a5728f1
Sam <burtman.samuel26@gmail.com>
2024-03-15 13:46:56 +0200

    update src/pages/notFound/NotFound.jsx

--- react-website-2/src/pages/notFound/NotFound.jsx
+++ react-website-2/src/pages/notFound/NotFound.jsx
@@ -1,12 +1,14 @@
 import {Link} from 'react-router-dom'
 import './notFound.css'
+import { useTranslation } from 'react-i18next'
 
 const NotFound = () => {
+  const { t } = useTranslation()
   return (
     <section>
       <div className="container notfound__container">
-        <h2>Page Not Found</h2>
-        <Link to="/" className='btn'>Go Back Home</Link>
+        <h2>{t('Page Not Found')}</h2>
+        <Link to="/" className='btn'>{t('Go Back Home')}</Link>
       </div>
     </section>
   )
