ff76d171b1747f130c38eb8d8b8d0b3c7c180ed7
Sam <burtman.samuel26@gmail.com>
2024-03-15 13:46:04 +0200

    update src/components/Footer.jsx

--- react-website-2/src/components/Footer.jsx
+++ react-website-2/src/components/Footer.jsx
@@ -4,11 +4,12 @@ import { FaLinkedin } from 'react-icons/fa'
 import { FaFacebookF } from 'react-icons/fa'
 import { AiOutlineTwitter } from 'react-icons/ai'
 import { AiFillInstagram } from 'react-icons/ai'
-
+import { useTranslation } from 'react-i18next'
 
 
 
 const Footer = () => {
+    const { t } = useTranslation()
   return (
     <footer>
         <div className="container footer__container">
@@ -17,7 +18,7 @@ const Footer = () => {
                     <img src={Logo} alt="Footer Logo" />
                 </Link>
                 <p>
-                Our Socials and contacts.
+                {t('Our Socials and contacts.')}
                 </p>
                 <div className="footer__socials">
                     <a href="https://linkedin.com/" target="_blank" rel='noreferrer noopener'><FaLinkedin/></a>
@@ -27,18 +28,18 @@ const Footer = () => {
                 </div>
             </article>
             <article>
-                <h4 className='permalinks'> Permalinks</h4>
-                <Link to="/catalog">Catalog</Link>
-                <Link to="/contact">Contact</Link>
+                <h4 className='permalinks'>{t('Permalinks')}</h4>
+                <Link to="/catalog">{t('Catalog')}</Link>
+                <Link to="/contact">{t('Contact')}</Link>
             </article>
             <article>
-                <h4 className='Insights'>Insights</h4>
-                <Link to="/s">Blog</Link>
-                <Link to="/s">Communities</Link>
+                <h4 className='Insights'>{t('Insights')}</h4>
+                <Link to="/s">{t('Blog')}</Link>
+                <Link to="/s">{t('Communities')}</Link>
             </article>
             <article>
-                <h4 className='get-in-touch'>Get In Touch</h4>
-                <Link to="/contact">Contact Us</Link>
+                <h4 className='get-in-touch'>{t('Get In Touch')}</h4>
+                <Link to="/contact">{t('Contact Us')}</Link>
             </article>
         </div>
         <div className="footer__copyright">
