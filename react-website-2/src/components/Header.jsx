98516cb2f3d8e6bb1bc91f916a085ae4c9e1c89e
Sam <burtman.samuel26@gmail.com>
2024-03-16 21:47:14 +0200

    update src/components/Header.jsx

--- react-website-2/src/components/Header.jsx
+++ react-website-2/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({title, image, children}) => {
     <header className="header">
         <div className="header__container">
             <div className="header__container-bg">
-                <img src={image} alt="Header Background Image" />
+                <img src={image} alt="Header Background" />
             </div>
             <div className="header__content">
                 <h2>{title}</h2>
