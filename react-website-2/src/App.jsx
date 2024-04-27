d4c61dfd8bbc4a60a5b5dc7b7e15211eff83e873
MostOfLuck <burtman.samuel26@gmail.com>
2024-04-27 11:38:15 +0200

    update react-website-2/src/App.jsx

--- react-website-2/src/App.jsx
+++ react-website-2/src/App.jsx
@@ -1,26 +1,52 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home/Home'
-import Contact from './pages/contact/Contact'
-import NotFound from './pages/notFound/NotFound'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Catalogs from './pages/trainers/Trainers'
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Contact from './pages/contact/Contact';
+import NotFound from './pages/notFound/NotFound';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Catalogs from './pages/trainers/Trainers';
+import './index.css';
 
-
+const Modal = ({ onUnder18Click }) => (
+  <div className="modal-overlay">
+    <div className="modal">
+      <div className="modal-content">
+        <h2>Welcome!</h2>
+        <p>Verify that you are legal age.</p>
+        <button className='button1' onClick={() => onUnder18Click(false)}>I'm above 18</button>
+        <button className='button2' onClick={() => onUnder18Click(true)}>I'm under 18</button>
+      </div>
+    </div>
+  </div>
+);
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<Navbar />
-			<Routes>
-				<Route index element={<Home />} />
-				<Route path='contact' element={<Contact />} />
-				<Route path='catalog' element={<Catalogs />} />
-				<Route path='*' element={<NotFound />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	)
-}
+  const [isOver18, setIsOver18] = useState(null);
+  const [modalShown, setModalShown] = useState(false);
 
-export default App
+  const handleAgeClick = (isUnder18) => {
+    setIsOver18(!isUnder18);
+    setModalShown(true);
+    if (isUnder18) {
+      window.location.href = "/NotFound";
+    }
+  };
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/catalog' element={<Catalogs />} />
+        <Route path='/NotFound' element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      <Footer />
+      {isOver18 === null && !modalShown && <Modal onUnder18Click={handleAgeClick} />}
+    </BrowserRouter>
+  );
+};
+
+export default App;
