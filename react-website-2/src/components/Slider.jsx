3f421a23b08ca0d0644e0d5aef14f128485e2639
MostOfLuck <burtman.samuel26@gmail.com>
2024-04-28 18:48:23 +0200

    update  react-website-2/src/components/Slider.jsx

--- react-website-2/src/components/Slider.jsx
+++ react-website-2/src/components/Slider.jsx
@@ -3,13 +3,14 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './slider.css';
-import logo1 from '../productImages/logoadega.jpeg'
+import logo1 from '../productImages/logoadega.jpeg.png'
 import logo2 from '../productImages/logofiuza.jpeg'
 import logo3 from '../productImages/logotrijol.jpeg'
 import logo4 from '../productImages/logoalonseason.jpg'
 import logo5 from '../productImages/logoporto.jpg'
 import logo6 from '../productImages/Nativereservalogo.png'
 import logo7 from '../productImages/oceaniuslogo.jpg'
+import logo8 from '../productImages/Galitos.jpeg'
 
 const logos = [
   { id: 1, src: logo1, alt: 'Logo 1' },
@@ -19,6 +20,7 @@ const logos = [
 	{ id: 5, src: logo5, alt: 'Logo 5' },
 	{ id: 6, src: logo6, alt: 'Logo 6' },
 	{ id: 7, src: logo7, alt: 'Logo 7' },
+  { id: 8, src: logo8, alt: 'Logo 8' },
   // Добавьте любое количество логотипов
 ];
 
