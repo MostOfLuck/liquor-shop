ed04d01e56cbcff667169797ad09f907a5e966d2
MostOfLuck <burtman.samuel26@gmail.com>
2024-08-11 15:25:55 +0300

    update

--- react-website-2/src/components/Testimonials.jsx
+++ react-website-2/src/components/Testimonials.jsx
@@ -5,6 +5,7 @@ import Card from "../UI/Card";
 import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from 'react-icons/io';
 import { testimonials } from "../data";
 import { useTranslation } from 'react-i18next';
+import ChatBot from './ChatBot'
 
 const Testimonials = () => {
     const [index, setIndex] = useState(0);
@@ -27,6 +28,7 @@ const Testimonials = () => {
 
     return (
         <section className="testimonials">
+                    <ChatBot/>
             <div className="container testimonials__container">
                 <SectionHead icon={<ImQuotesLeft />} title={t('Reviews')} className="testimonials__head" />
                 <Card className="testimonial">
